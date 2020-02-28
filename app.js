2691ba564d8df6dd529ad4f2951e926167140c0e
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-02-28 14:33:27 -0500

    code structure

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(logger('dev'));
+
+// Controllers
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'index page'
+    });
+});
+
+
+// catch 404 error
+app.use((req, res, next) => {
+    const err = new Error('not found');
+    err.status = 404;
+    next(err);
+});
+
+
+// Error handler function
+app.use((err, req, res,next) => {
+    // set locals, only providing error in development
+    const error = app.get('env') === 'development' ? err : {};
+    const status = error.status || 500;
+    
+    // respond to clients
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    // response to me
+    console.error(err);
+})
+
+// start the server
+
+app.listen(2222,() => {
+    console.log('the server is listening on port 2222');
+})
